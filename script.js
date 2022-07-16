3d40674e5150c9d928b257d34514206e330e46f9
Greg <greg19850@gmail.com>
2022-07-16 11:26:11 +0100

    created interval, with function switching slides

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-const sliderImg = document.querySelector('img.slider');
-const sliderText = document.querySelector('h1.slider');
+const imgDisplay = document.querySelector("img.slider");
+const textDisplay = document.querySelector("h1.slider");
+
+let time = 3000;
+let slideIndex = 0;
 
 const sliderOptions = [{
   img: "img/img1.jpg",
   text: "First text"
 },
-
 {
-  img: "img/img1.jpg",
-  text: "First text"
+  img: "img/img2.jpg",
+  text: "Second text"
 },
 {
-  img: "img/img1.jpg",
-  text: "First text"
-}]
+  img: "img/img3.jpg",
+  text: "Third text"
+}];
+
+const switchSlide = () => {
+  slideIndex++;
+
+  if(slideIndex === sliderOptions.length) slideIndex = 0;
+
+  imgDisplay.src = sliderOptions[slideIndex].img;
+  textDisplay.textContent = sliderOptions[slideIndex].text;
+
+}
+
+setInterval(switchSlide, time)
