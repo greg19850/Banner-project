d01129b55a190177d1d73314e2db5232ebe2c85b
Greg <greg19850@gmail.com>
2022-07-16 12:18:03 +0100

    created function switching active dot on bottom of screen, along with changing slides

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
-const imgDisplay = document.querySelector("img.slider");
-const textDisplay = document.querySelector("h1.slider");
+const imgDisplay = document.querySelector('img.slider');
+const textDisplay = document.querySelector('h1.slider');
 
 let time = 3000;
-let slideIndex = 0;
+let activeIndex = 0;
+const dots = [...document.querySelectorAll('.dots span')];
 
-const sliderOptions = [{
-  img: "img/img1.jpg",
-  text: "First text"
-},
-{
-  img: "img/img2.jpg",
-  text: "Second text"
-},
-{
-  img: "img/img3.jpg",
-  text: "Third text"
-}];
+const sliderOptions = [
+  {
+    img: 'img/img1.jpg',
+    text: 'First text',
+  },
+  {
+    img: 'img/img2.jpg',
+    text: 'Second text',
+  },
+  {
+    img: 'img/img3.jpg',
+    text: 'Third text',
+  },
+];
+
+const changeActiveDot = () => {
+  let dotIndex = dots.findIndex((dot) => dot.classList.contains('active'));
+  dots[dotIndex].classList.remove('active');
+  dots[activeIndex].classList.add('active');
+};
 
 const switchSlide = () => {
-  slideIndex++;
+  activeIndex++;
 
-  if(slideIndex === sliderOptions.length) slideIndex = 0;
+  if (activeIndex === sliderOptions.length) activeIndex = 0;
 
-  imgDisplay.src = sliderOptions[slideIndex].img;
-  textDisplay.textContent = sliderOptions[slideIndex].text;
+  imgDisplay.src = sliderOptions[activeIndex].img;
+  textDisplay.textContent = sliderOptions[activeIndex].text;
 
-}
+  changeActiveDot();
+};
 
-setInterval(switchSlide, time)
+setInterval(switchSlide, time);
