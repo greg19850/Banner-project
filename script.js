d760a1f4ad27a5606d651838e2dbd489a03bea7e
Greg <greg19850@gmail.com>
2022-07-16 11:07:08 +0100

    created variables for slide txt and img, creted array with objects holding img and txt info

---
+++ script.js
@@ -0,0 +1,16 @@
+const sliderImg = document.querySelector('img.slider');
+const sliderText = document.querySelector('h1.slider');
+
+const sliderOptions = [{
+  img: "img/img1.jpg",
+  text: "First text"
+},
+
+{
+  img: "img/img1.jpg",
+  text: "First text"
+},
+{
+  img: "img/img1.jpg",
+  text: "First text"
+}]
